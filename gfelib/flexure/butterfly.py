912cda5d7e6d370f582f92e0a63abf78848c7b76
Daniel He <me@daniel-egg.com>
2025-12-06 13:44:34 -0800

    Refactor flexure.butterfly

--- gfelib/flexure/butterfly.py
+++ gfelib/flexure/butterfly.py
@@ -7,34 +7,35 @@ import gfelib as gl
 
 @gf.cell_with_module_name
 def butterfly(
-    radius_outer: float,
     radius_inner: float,
-    width_beam: float,
+    radius_outer: float,
     width_inner: float,
-    angles: tuple[float, float] = (30.0, 60.0),
-    thicken: bool = False,
-    thick_ratio: float = 0,
-    thick_width: float = 0,
-    release_hole_on_inner_carriage: bool = True,
-    angle_resolution: float = 1,
-    geometry_layer: gf.typings.LayerSpec = 0,
-    release_hole_radius: float = 0,
-    release_distance: float = 1,
-    release_layer: gf.typings.LayerSpec = 1,
+    width_beam: float,
+    angles: tuple[float, float],
+    geometry_layer: gf.typings.LayerSpec,
+    thick_length: float,
+    thick_width: float,
+    thick_offset: float,
+    release_inner: bool,
+    angle_resolution: float,
+    release_hole_radius: float,
+    release_distance: float,
+    release_layer: gf.typings.LayerSpec,
 ) -> gf.Component:
     """Returns a half-butterfly joint (4 beams)
 
     Args:
-        radius_outer: OD of the joint
-        radius_inner: ID of the joint
-        angles: 2-tuple of the beam angles (typically (30, 60))
-        width_beam: width of thin part of beam
-        width_inner: width of inner intermediate stage
-        thicken: whether to thicken middle part of beam
-        thick_ratio: ratio of thickened part
-        thick_width: width of thickened part
-        release_hole_on_inner_carriage: whether to hollow the inner intermediate stage
-        geometry_layer: layer to place device
+        radius_inner: inner radius of the inner carriage
+        radius_outer: outer radius of the joint
+        width_inner: width of the inner carriage
+        width_beam: width of the beams
+        angles: angles to place beams
+        geometry_layer: layer to place polygon
+        thick_length: length of beam thick section
+        thick_width: width of beam thick section
+        thick_offset: offset of beam thick section, positive is away from center
+        release_inner: `True` to release inner carriage
+        angle_resolution: number of degrees per point
         release_hole_radius: radius of the release holes
         release_distance: maximum distance between adjacent release holes
         release_layer: layer to place release holes
@@ -43,53 +44,38 @@ def butterfly(
 
     angles = sorted(angles)
 
-    # Emit inner intermediate stage
-    rmid_inner = radius_inner + width_inner / 2
-    start_angle = angles[0] - width_beam / 2 / (radius_inner + width_inner) / (
+    angle_start = angles[0] - 0.5 * width_beam / (radius_inner + width_inner) / (
         np.pi / 180
     )
-    end_angle = 180 - start_angle
-    if release_hole_on_inner_carriage:
-        ref = c << gl.basic.ring(
-            radius=rmid_inner,
-            width=width_inner,
-            angle=end_angle - start_angle,
-            geometry_layer=geometry_layer,
-            release_distance=release_distance,
-            release_hole_radius=release_hole_radius,
-            release_layer=release_layer,
-            angle_resolution=angle_resolution,
-        )
-    else:
-        ref = c << gf.components.ring(
-            radius=rmid_inner,
-            width=width_inner,
-            angle=end_angle - start_angle,
-            layer=geometry_layer,
-            angle_resolution=angle_resolution,
-        )
-    ref.rotate(start_angle, (0, 0))
 
-    # Emit four beams
-    length_beam = (
-        radius_outer - radius_inner - width_inner + width_beam / 2
-    )  # Compensate difference between arc and line
-    rmid_beam = (radius_outer + radius_inner + width_inner) / 2
-    beam = gl.flexure.beam(
-        length=length_beam,
-        width=width_beam,
-        thicken=thicken,
-        thick_ratio=thick_ratio,
-        thick_width=thick_width,
+    inner_carriage_ref = c << gl.basic.ring(
+        radius=radius_inner + 0.5 * width_inner,
+        width=width_inner,
+        angle=180 - 2 * angle_start,
         geometry_layer=geometry_layer,
+        angle_resolution=angle_resolution,
+        release_hole_radius=release_hole_radius if release_inner else 0,
+        release_distance=release_distance if release_inner else 0,
         release_layer=release_layer,
+    )
+    inner_carriage_ref.rotate(angle_start, (0, 0))
+
+    beam_offset = 0.5 * (radius_outer + radius_inner + width_inner)
+    beam = gl.flexure.beam(
+        length=radius_outer - radius_inner - width_inner + 0.5 * width_beam,
+        width=width_beam,
+        geometry_layer=geometry_layer,
+        thick_length=thick_length,
+        thick_width=thick_width,
+        thick_offset=thick_offset,
         release_hole_radius=release_hole_radius,
         release_distance=release_distance,
+        release_layer=release_layer,
     )
-    (c << beam).move((rmid_beam, 0)).rotate(angles[0], (0, 0))
-    (c << beam).move((rmid_beam, 0)).rotate(angles[1], (0, 0))
-    (c << beam).move((rmid_beam, 0)).rotate(180 - angles[0], (0, 0))
-    (c << beam).move((rmid_beam, 0)).rotate(180 - angles[1], (0, 0))
+    for a in [angles[0], angles[1], 180 - angles[1], 180 - angles[0]]:
+        ref = c << beam
+        ref.move((beam_offset, 0)).rotate(a, (0, 0))
 
     c.flatten()
+
     return c
