e3faa2a85af9c4fd703193e553215fc92300becb
caoyuan96421 <caoyuan96421@gmail.com>
2025-12-06 19:54:28 -0800

    Added polygon with release holes. Implementation could be improved in the future

--- gfelib/basic/__init__.py
+++ gfelib/basic/__init__.py
@@ -3,3 +3,4 @@ from __future__ import annotations
 from gfelib.basic.circle import circle
 from gfelib.basic.rectangle import rectangle
 from gfelib.basic.ring import ring, ring_span, ring_full
+from gfelib.basic.polygon import polygon
--- gfelib/basic/polygon.py
+++ gfelib/basic/polygon.py
@@ -1,51 +1,60 @@
+from __future__ import annotations
+
 import gdsfactory as gf
 
 import numpy as np
 
+import gfelib as gl
+
 
 @gf.cell_with_module_name
 def polygon(
-    points: tuple[tuple[float, float], ...] | list | np.ndarray,
+    points: tuple[tuple[float, float], ...],
     geometry_layer: gf.typings.LayerSpec,
-    release_hole_radius: float,
-    release_distance: float,
-    release_layer: gf.typings.LayerSpec,
+    release_spec: gl.datatypes.ReleaseSpec | None,
 ) -> gf.Component:
     """Returns a rectangle with release holes
 
     Args:
         points: points of polygon (doesn't need to repeat the first point)
         geometry_layer: layer to place polygon
-        centered: `True` sets center to (0, 0), `False` sets south-west to (0, 0)
-        release_hole_radius: radius of the release holes
-        release_distance: maximum distance between adjacent release holes
-        release_layer: layer to place release holes
+        release_specs: release specification
     """
     c = gf.Component()
+
     shape = c.add_polygon(points, geometry_layer)
 
-    if not shape:
+    if not release_spec or not shape.is_polygon():
         return c
 
+    # Shrink by release_distance
+    reduced = c.get_region(layer=geometry_layer, merge=True)
+
+    # .sized(
+    #     -c.kcl.to_dbu(release_spec.hole_radius)
+    # )
+
     bb = c.kcl.to_um(shape.bbox())  # Get bounding box
+    size = [bb.width(), bb.height()]
+    origin = [bb.left, bb.bottom]
+    print(origin, size)
 
-    hole = gf.components.circle(
-        radius=release_hole_radius,
-        layer=release_layer,
-    )
+    if not release_spec.released:
+        return c
 
-    max_dist = 2 * (release_hole_radius + release_distance) / np.sqrt(2)
-    step_x = bb.width / np.ceil(bb.width / max_dist)
-    step_y = bb.height / np.ceil(bb.height / max_dist)
+    if size[0] <= release_spec.distance or size[1] <= release_spec.distance:
+        return c
 
-    for y in np.arange(0.5 * step_y, bb.height, step_y):
-        for x in np.arange(0.5 * step_x, bb.width, step_x):
-            ref = c << hole
-            ref.move(
-                (
-                    x - (0.5 * bb.width),
-                    y - (0.5 * bb.height),
-                )
-            )
+    s = 2 * (release_spec.hole_radius + release_spec.distance) / np.sqrt(2)
+    sx = size[0] / np.ceil(size[0] / s)
+    sy = size[1] / np.ceil(size[1] / s)
+
+    for y in np.arange(origin[1] + 0.5 * sy, origin[1] + size[1], sy):
+        for x in np.arange(origin[0] + 0.5 * sx, origin[0] + size[0], sx):
+            pt = c.kcl.to_dbu(gf.kdb.DPoint(x, y))
+
+            if any(p.inside(pt) for p in reduced.each()):
+                ref = c << release_spec.hole
+                ref.move((x, y))
 
     return c
